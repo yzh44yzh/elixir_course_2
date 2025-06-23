1670497f11de7dfd585c9025fd746853486b7d10
Yuriy Zhloba <y.zhloba@erlyvideo.org>
2025-06-23 23:46:45 +0300

    lesson 01 presentation

--- lesson_01/lib/processes.exs
+++ lesson_01/lib/processes.exs
@@ -1,13 +1,12 @@
 defmodule ProcessesExample do
   def run_processes(num) do
-    IO.puts("__MODULE__:")
-    IO.inspect(__MODULE__)
     for id <- 1..num, do: spawn(__MODULE__, :start, [id])
   end
 
   def start(id) do
     IO.puts("Process id:#{id} pid:#{inspect(self())} started")
     Process.sleep(Enum.random(10..500))
+    send(parent_pid, {:hello, id, self()})
     IO.puts("Process id:#{id} pid:#{inspect(self())} stopped")
   end
 end
